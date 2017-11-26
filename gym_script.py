a0227392fcec5f322f756e98186e258802f973e3
cedric <cedric.oeldorf@yahoo.de>
2017-11-26 17:13:07 +0100

    implementation of q learning

--- gym_script.py
+++ gym_script.py
@@ -1,50 +1,59 @@
 
 import gym
 import numpy as np
+
 slow = True
 env = gym.make("MountainCar-v0")
 
-#############
-# Calculate maximum/minimum position and speed for discretization
-#############
-max_position = env.observation_space.high[0]
-min_position = env.observation_space.low[0]
-max_speed = env.observation_space.high[1]
-min_speed = env.observation_space.low[1]
-
-NUM_BUCKETS = (10, 10, 14, 14)
-NUM_ACTIONS = env.action_space.n
-ACTION_INDEX = len(NUM_BUCKETS)
-
-###########
-## table to fill in
-############
-q_table = np.zeros(NUM_BUCKETS + (NUM_ACTIONS,))
-
-for _ in range(0,10):
-    observation = env.reset()
-    done = False
-    timesteps = 0
-    while not done:
-        if slow: env.render()
-
-        # Decide on an action
-        action = env.action_space.sample() # your agent here (this takes random actions)
-
-        # take action
-        observation, reward, done, info = env.step(action)
-        timesteps+=1
-
-        if slow: print(observation)
-        if slow: print(reward)
-        if slow: print(done)
-    print("Episode finished after ", timesteps, "timesteps.")
+position_start = env.observation_space.low[0]
+position_end = env.observation_space.high[0]
+speed_start = env.observation_space.low[1]
+speed_end = env.observation_space.high[1]
+number_of_bins = 20
 
 
-# what we need to do? (maybe?)
-#
+def discretize(start, end, bins):
+    bin_width = (end - start) / bins
+    return_array = [start]
+    for i in range(1, bins):
+        return_array.append(start + i*bin_width)
+    return_array.append(end)
+    return return_array
 
-## value iteration
-# - evaluate rewards and transitions
-# - Create Q
-# - update policy
+position_bins = discretize(position_start, position_end, number_of_bins)
+speed_bins = discretize(speed_start, speed_end, number_of_bins)
+states = []
+for i in position_bins:
+    for j in speed_bins:
+      states.append(np.array([i,j]))
+states = np.array(states)
+
+def value_to_state(x, states):
+    pos = []
+    speed = []
+    for s in states:
+        
+
+
+    closest_position = min(states, key=lambda x:abs(x-myNumber))xpos=np.digitize(x[0],pos_bins)
+    xvel=np.digitize(x[1],vel_bins)
+    stateValue = np.array([pos_bins[xpos],vel_bins[xvel]])
+    state = np.where((allStates==stateValue).all(axis=1))[0][0]
+    return state
+
+def find_in_states (x, states):
+    for i in states[0]
+
+
+
+
+
+
+
+
+
+
+
+
+print(position_bins)
+print(speed_bins)
